e696a890e66fddf8f25c81bb8b4aa873ad233a4a
Manish Tiwari <manishtiwari2498@gmail.com>
2025-04-12 18:57:22 +0530

    Initial commit

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Books are more than just pages—they're windows into new worlds,
+            ideas, and perspectives. From thrilling adventures and heartwarming
+            tales to deep insights and timeless wisdom, every book holds a
+            journey waiting to unfold. Whether you're a casual reader or a
+            passionate bibliophile, there's always a story ready to inspire,
+            educate, or entertain you. Dive in and let your imagination soar.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
